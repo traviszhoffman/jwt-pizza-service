434f8163dedb36682547a6686e1c5f666f7f15ef
Travis Hoffman <traviszhoffman@gmail.com>
2026-02-03 18:46:17 -0700

    updated eslint config to better match config in tutorial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.test.js", "**/*.spec.js"], languageOptions: { globals: globals.jest } },
-]);
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  { files: ['**/*.test.js', '**/*.spec.js'], languageOptions: { globals: globals.jest } },
+  pluginJs.configs.recommended,
+];
