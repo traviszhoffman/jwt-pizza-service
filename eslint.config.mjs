5bfb9d8cda031fc9f152de969fbcc48d2ea17026
Travis Hoffman <traviszhoffman@gmail.com>
2026-04-01 10:37:01 -0600

    fix for linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
+  { ignores: ['loadtests/loginAndOrder.js'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.node } },
   { files: ['**/*.test.js', '**/*.spec.js'], languageOptions: { globals: globals.jest } },
